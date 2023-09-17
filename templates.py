302e837f83660b4116a1a5ca35ec2cd130d09760
prumpet <cerufif@gmail.com>
2023-09-16 22:42:10 -0400

    Finished Version 0.1

--- templates.py
+++ templates.py
@@ -1,1 +1,15 @@
 import os
+import sys
+
+templateFile = sys.argv[1]
+
+if templateFile.endswith(".template"):
+    with open(templateFile, 'r+') as template:
+        templateContents = template.read()
+        template.close()
+
+    fileName = templateFile.strip('.template')
+
+    file = open('generatedTemplate.py', 'w+')
+
+    file.write(templateContents)
